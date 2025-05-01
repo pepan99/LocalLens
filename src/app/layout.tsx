f5f7376c9af818a1ce245e3d72122fc3635c9425
Milos Hegr <milos.hegr@lifeliqe.com>
2025-05-01 20:25:12 +0200

    feat: add basic leaflet map

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { MapBackground } from "@/components/map/map-background";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,10 +26,14 @@ const RootLayout = ({
 }>) => {
   return (
     <html lang="en">
+      <head>
+        <title>LocalLens</title>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <MapBackground />
       </body>
     </html>
   );
