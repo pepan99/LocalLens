7e15eb44bad323aff4ba9504c46bce1efc81bd8e
xkosvan <xkosvan@fi.muni.cz>
2025-05-12 15:11:49 +0200

    revert: add friend using qr code

--- src/components/friends/add-friend-dialog.tsx
+++ src/components/friends/add-friend-dialog.tsx
@@ -68,26 +68,6 @@ const AddFriendDialog = ({
           >
             Send Friend Request
           </Button>
-
-          <Separator />
-
-          <div className="space-y-2">
-            <h3 className="text-sm font-medium">QR Code</h3>
-            <p className="text-sm text-gray-500">
-              Have your friend scan your unique QR code to connect
-            </p>
-            <div className="bg-gray-100 aspect-square rounded-lg flex items-center justify-center p-4">
-              <div className="w-48 h-48 bg-gray-300 rounded-md flex items-center justify-center">
-                <p className="text-gray-600 text-sm">QR Code Placeholder</p>
-              </div>
-            </div>
-            <div className="flex justify-center">
-              <Button variant="outline" size="sm">
-                <Share className="h-4 w-4 mr-2" />
-                Share Profile Link
-              </Button>
-            </div>
-          </div>
         </div>
 
         <DialogFooter className="sm:justify-end mt-4">
